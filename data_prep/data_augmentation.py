f234325b80a691bc011a5960d12a46b8007fc1cb
AndreyLebedinskiy <avlebedinskiy@edu.hse.ru>
2025-05-09 15:38:49 +0300

    modified augmentation to work with datasets lacking masks

--- data_prep/data_augmentation.py
+++ data_prep/data_augmentation.py
@@ -9,12 +9,22 @@ transform = tio.Compose([
     tio.RandomGamma(log_gamma=(0.9, 1.01), p=0.7),
 ])
 
+def augment_scan_only(image_path, output_dir, num_aug=2):
+    print("augmenting scan only:", image_path)
+    image = tio.ScalarImage(image_path)
+    subject = tio.Subject(image=image)
+
+    for i in range(num_aug):
+        augmented = transform(subject)
+        base = os.path.basename(image_path).replace('.nii.gz', f'_aug{i}.nii.gz')
+        augmented.image.save(os.path.join(output_dir, 'scans', base))
+
 def augment_scan_mask_pair(image_path, mask_path, output_dir, num_aug=2):
-    print("augmenting:", image_path)
+    print("augmenting scan+mask:", image_path)
     image = tio.ScalarImage(image_path)
     mask = tio.LabelMap(mask_path)
     subject = tio.Subject(image=image, mask=mask)
-    
+
     for i in range(num_aug):
         augmented = transform(subject)
         base = os.path.basename(image_path).replace('.nii.gz', f'_aug{i}.nii.gz')
@@ -25,26 +35,25 @@ def augment_scan_mask_pair(image_path, mask_path, output_dir, num_aug=2):
 
 def augment_dataset(scan_folder, mask_folder, output_folder, num_aug=2):
     os.makedirs(os.path.join(output_folder, 'scans'), exist_ok=True)
-    os.makedirs(os.path.join(output_folder, 'masks'), exist_ok=True)
+    if mask_folder:
+        os.makedirs(os.path.join(output_folder, 'masks'), exist_ok=True)
+    print("Augmenting folder:", scan_folder)
 
     scan_paths = sorted(glob(os.path.join(scan_folder, '*.nii.gz')))
-    mask_paths = sorted(glob(os.path.join(mask_folder, '*.nii.gz')))
-    
-    if len(scan_paths) != len(mask_paths):
-        print("Mismatch in number of scans and masks")
-        print(len(scan_paths), len(mask_paths))
-        return
+    mask_paths = []
+    if mask_folder: 
+        mask_paths = sorted(glob(os.path.join(mask_folder, '*.nii.gz')))
+    mask_dict = {os.path.basename(p): path for path in mask_paths}
 
-    for scan_path, mask_path in zip(scan_paths, mask_paths):
-        augment_scan_mask_pair(scan_path, mask_path, output_folder, num_aug=num_aug)
+    for scan_path in scan_paths:
+        scan_fname = os.path.basename(scan_path)
+        mask_path = mask_dict.get(scan_fname)
 
+        if mask_path:
+            augment_scan_mask_pair(scan_path, mask_path, output_folder, num_aug=num_aug)
+        else:
+            augment_scan_only(scan_path, output_dir=output_folder, num_aug=num_aug)
 
-augment_dataset(
-    scan_folder='segmentation_of_lung_CT_scans/data/preprocesd/MMWHS_data/scans',
-    mask_folder='segmentation_of_lung_CT_scans/data/preprocesd/MMWHS_data/heart_masks',
-    output_folder='segmentation_of_lung_CT_scans/data/augmented/MMWHS',
-    num_aug=2
-)
 
 augment_dataset(
     scan_folder='segmentation_of_lung_CT_scans/data/preprocesd/luna16/scans',
